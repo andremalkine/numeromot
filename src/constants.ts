9f60f666a6a5100c5dc34c327143a1c84a554e37
Andre Malkine <andre.malkine@fgh.com>
2022-12-16 10:22:52 -0500

    Create optimized dictionary JSON.

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const partsOfSpeech = {
+  'n.': 'noun',
+  'prep.': 'preposition',
+  'a.': 'adjective',
+  'v.': 'verb',
+  'adv.': 'adverb',
+  'p.': 'past-participle',
+  'interj.': 'interjection',
+  'conj.': 'conjunction',
+  'pron.': 'pronoun'
+}
